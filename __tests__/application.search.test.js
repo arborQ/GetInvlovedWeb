8f925f29efe7f6a0e79f040571fb3ceb4a132427
Lukasz Wojcik <lukasz.wojcik@smtsoftware.com>
2016-03-30 16:02:57 +0200

    coverage

--- __tests__/application.search.test.js
+++ __tests__/application.search.test.js
@@ -1,8 +1,21 @@
-jest.unmock('../sources/components/search/fake');
+jest.unmock('../sources/components/search/application.search');
+jest.unmock('../sources/components/routing/routes');
+
+import searchComponent from '../sources/components/search/application.search';
 
 describe('application.search', () => {
-  it('has render function', () => {
-    var searchComponent = require('../sources/components/search/fake');
+  it('is defined', () => {
     expect(searchComponent).toBeDefined();
+  });
+
+  it('can create instance, and has render function', () => {
+    var instance = new searchComponent();
+    expect(instance).not.toBe(null);
+    expect(instance.render).toBeDefined();
+  });
+
+  it('can render', () => {
+    var instance = new searchComponent();
+    instance.render();
   });
 });
