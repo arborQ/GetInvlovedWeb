af021a5b7e32b85c98ff806d65203ffbce729c01
Lukasz Wojcik <lukasz.wojcik@smtsoftware.com>
2016-03-30 15:29:31 +0200

    const var

--- __tests__/application.search.test.js
+++ __tests__/application.search.test.js
@@ -2,7 +2,7 @@ jest.unmock('../sources/components/search/fake');
 
 describe('application.search', () => {
   it('has render function', () => {
-    const searchComponent = require('../sources/components/search/fake');
+    var searchComponent = require('../sources/components/search/fake');
     expect(searchComponent).toBeDefined();
   });
 });
