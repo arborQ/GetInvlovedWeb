3db615c628e1c106e662724e88aadb5f51805061
arbor <arbor@o2.pl>
2016-04-19 21:22:35 +0200

    menu items

--- sources/frontEnd/components/Application/aplication.menu.jsx
+++ sources/frontEnd/components/Application/aplication.menu.jsx
@@ -13,7 +13,11 @@ export default class applicationMenu extends React.Component{
     return (
         <DropDownContainer triggerItem={<div className="account-settings"></div>}>
           <Paper>
-            <div>tesfdfdsft</div>
+            <div className="vertical-menu" style={{width : '200px'}}>
+              <Link to="/userSettings">Ustawienia</Link>
+              <Link to="/help">Help</Link>
+              <Link to="/logout">Wyloguj</Link>
+            </div>
           </Paper>
         </DropDownContainer>
     );
