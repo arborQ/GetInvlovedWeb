98e1082a0022532d14f3c5dfd2c760adc6d6735b
Lukasz Wojcik <lukasz.wojcik@smtsoftware.com>
2016-06-01 12:01:38 +0200

    gdf

--- sources/frontEnd/components/signIn/signIn.component.jsx
+++ sources/frontEnd/components/signIn/signIn.component.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { assign } from 'lodash';
 import { SignInAction, DisplayMessageAction } from 'flux-actions'
 import { RouteItem } from 'routing';
-import { ButtonContainer, InputContainer, FormContainer } from 'ui';
+import { ButtonContainer, InputContainer, FormContainer, Paper } from 'ui';
 import { NavigateTo } from 'routing';
 import { post } from 'api-call';
 
@@ -25,7 +25,6 @@ export default class signInComponent extends React.Component{
       if(res.isAuthenticated){
         SignInAction(res.token);
         DisplayMessageAction("Sign in successfull");
-        console.log('DisplayMessageAction');
       }else{
         DisplayMessageAction(res.message);
       }
@@ -35,13 +34,13 @@ export default class signInComponent extends React.Component{
   render(){
     var isValid = this.formIsValid();
     return (
-      <div className="mui-panel">
+      <Paper>
         <FormContainer onSubmit={this.submitForm.bind(this)}>
           <InputContainer type="text" id="email" placeholder="Email" value={this.state.login} onChange={(e) => { this.setState(assign({}, this.state, { login : e.target.value })) }} />
           <InputContainer type="password" id="password" placeholder="Password" value={this.state.password} onChange={(e) => { this.setState(assign({}, this.state, { password : e.target.value })) }} />
           <ButtonContainer disabled={!this.formIsValid()} type="submit">Sign In</ButtonContainer>
         </FormContainer>
-      </div>
+      </Paper>
     );
   }
 }
